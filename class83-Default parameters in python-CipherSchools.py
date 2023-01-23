64d3fdba3e5b0faefb8fb27934fa069b93627415
Satyam <118934963+Programming01Pro@users.noreply.github.com>
2023-01-23 20:05:17 +0530

    Delete class83-Default parameters in python-CipherSchools.py

--- class83-Default parameters in python-CipherSchools.py
+++
@@ -1,5 +0,0 @@
-def user_info(first_name = "unkown", last_name = "unknown", age = None):
-    print(f"Your first name is {first_name}")
-    print(f"Your last name is {last_name}")
-    print(f"Your age is {age}")
-user_info("harshit",24)
